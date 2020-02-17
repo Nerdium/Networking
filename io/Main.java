c060026dd34312b09c14c5239ec339ebede18e99
Ben Johnson <nerdiumlol@gmail.com>
2020-02-17 09:33:06 -0600

    eclipse update

--- io/Main.java
+++ io/Main.java
@@ -1,9 +1,14 @@
 package io;
 
 import java.io.IOException;
+import java.util.Scanner;
+
+import io.network.client.Client;
+import io.network.event.MessageEvent;
+import io.network.server.Server;
 
 public class Main {
-  public static void main (String[] args) {
+  public static void main (String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Server or client: ");
 		if(sc.nextLine().equals("s")) {
